84b793089aec11e8e8772e70a3110791850b792f
Daniel Wood <daniel.j.wood11@gmail.com>
2018-04-18 17:51:04 -0400

    add some toggles

--- js/main.js
+++ js/main.js
@@ -1,20 +1,40 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//    $(this).children().addClass("hover") 
+//   }
+// }
+
+// filter dropdown actions
+$(".dropbtn").mouseover(function(){
+  $(this).children().addClass("hover")
+})
+$(".dropbtn").mouseout(function(){  
+  $(this).children().removeClass("hover")
+})
+
+$(".dropbtn").click(function(){
+  $(this).siblings().children().removeClass("active")
+  $(this).siblings().children().removeClass("active")
+  $(this).children().toggleClass("active")  
+})
+
+$(".option").click(function(){
+  $(this).toggleClass("active")
+  $(this).prev().toggleClass("active")
+})
